85934a03acd58cde0d6b26a247db8d8826e1ab27
Linnet Mbogo <linnetmbogo@Linnets-MacBook-Pro.local>
2024-10-23 12:44:14 -0400

    figuring out modules

--- controller/login_controller.go
+++ controller/login_controller.go
@@ -1,30 +1,12 @@
-package controller
+package login_controller
 
 import (
-	"net/http"
 	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
-// album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
-}
 func main() {
 	router := gin.Default()
 	router.Use(cors.New(cors.Config{
@@ -38,9 +20,7 @@ func main() {
 		},
 		MaxAge: 12 * time.Hour,
 	}))
-	// router.POST("/login", user_management.login)
-	// router.POST("/logout", user_management.logout)
-	// router.POST("/signup", user_management.signup)
+	router.POST("/login", user_management.login())
 
 	router.Run("localhost:8080")
 }
