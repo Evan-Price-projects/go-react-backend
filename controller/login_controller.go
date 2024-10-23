4febe59d86317231bc123f0a3f07bba63f7f35ec
Linnet Mbogo <linnetmbogo@Linnets-MacBook-Pro.local>
2024-10-23 12:17:02 -0400

    initial commit

---
+++ controller/login_controller.go
@@ -0,0 +1,46 @@
+package controller
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+// album represents data about a record album.
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+func main() {
+	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"PUT", "PATCH", "GET"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+	// router.POST("/login", user_management.login)
+	// router.POST("/logout", user_management.logout)
+	// router.POST("/signup", user_management.signup)
+
+	router.Run("localhost:8080")
+}
---
+++ main/user_management/go.mod
@@ -0,0 +1,3 @@
+module go-react/user_management
+
+go 1.23.2
