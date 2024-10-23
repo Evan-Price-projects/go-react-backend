3253809cca35bca57ed7193db35d0abbd07c4d28
Linnet Mbogo <eap2011@gmail.com>
2024-10-23 14:33:58 -0400

    added in signup

--- main/user_management/signup.go
+++ main/user_management/signup.go
@@ -1,7 +1,25 @@
 package user_management
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type SignupInfo struct {
+	Id        string    `json:"id"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	Birthdate time.Time `json:"birthdate"`
+}
 
 func Signup(c *gin.Context) {
-
+	var newSignupInfo SignupInfo
+	if err := c.BindJSON(&newSignupInfo); err != nil {
+		return
+	}
+	fmt.Print(newSignupInfo)
+	c.IndentedJSON(http.StatusCreated, newSignupInfo)
 }
